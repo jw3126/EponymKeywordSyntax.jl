c059dd621c4418121e2e03eb6d5d94802469f8b0
Jan Weidner <jw3126@gmail.com>
2019-12-11 23:24:36 -0800

    init

--- src/EponymKeywordSyntax.jl
+++ src/EponymKeywordSyntax.jl
@@ -1,5 +1,70 @@
 module EponymKeywordSyntax
+export @eponym
 
-greet() = print("Hello World!")
+using MacroTools: @capture, splitarg, MacroTools
+
+function resolve_kwarg(kwarg)
+    key, typ, splat, val = splitarg(kwarg)
+    if splat
+        kwarg
+    elseif val == nothing
+        :($key = $key)
+    else
+        kwarg
+    end
+end
+
+"""
+
+    @eponym(ex)
+
+The `@eponym` macro infers the name of a keyword argument
+from the name of its value. This is best explained by example:
+```jldoctest
+julia> using EponymKeywordSyntax
+
+julia> a=1; b=2
+2
+
+julia> f(args...; kw...) = (args=args, kw=kw)
+f (generic function with 1 method)
+
+julia> f(a=a, b=b) == @eponym f(;a,b)
+true
+
+julia> (a=a, b=b) == @eponym (;a, b)
+true
+
+julia> (a=a, b=10) == @eponym (;a, b=10)
+true
+
+julia> f(1, a=a, b=b) == @eponym f(1;a, b)
+true
+```
+"""
+macro eponym(ex)
+    esc(eponymmacro(ex))
+end
+
+function eponymmacro(ex)
+    ret = if @capture ex (f_(args__; kw__))
+        # function call
+        kw = map(resolve_kwarg, kw)
+        :($(f)($(args...); $(kw...)))
+    elseif @capture ex (;kw__)
+        kw = map(resolve_kwarg, kw)
+        # NamedTuple
+        :((;$(kw...)))
+    elseif Meta.isexpr(ex, :call) || Meta.isexpr(ex, :tuple)
+        ex
+    else
+        msg = """
+        Macro `@eponym` can only be applied to function calls, or `NamedTuple` calls
+        that begin with `;`.
+        Got: $(ex)
+        """
+        throw(ArgumentError(msg))
+    end
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,25 @@
-using EponymKeywordSyntax
 using Test
+using EponymKeywordSyntax
+const E = EponymKeywordSyntax
 
-@testset "EponymKeywordSyntax.jl" begin
-    # Write your own tests here.
+@testset "@eponym" begin
+    a = 1; b = 2
+    @test (A=:A, b=b) == @eponym((;A=:A, b))
+    @test (a=a, b=b) == @eponym((;a, b))
+    @test (b=b, a=a) == @eponym((;b, a))
+    kw = (c=3, d=4)
+    @test (;a=a, kw...) == @eponym((;a, kw...))
+    
+    f(args...; kw...) = (args=args, kw=kw)
+    
+    @test f(1,2) == @eponym f(1,2)
+    @test f(1,2; a=a, b=b) == @eponym f(1,2;a,b)
+    @test f(1,2; a=a, b=b) == @eponym f(1,2;a,b=b)
+    @test f(1,2; a=b, b=b)  == @eponym f(1,2;a=b,b)
+    kw = (c=3, d=4)
+    @test f(;kw...) == @eponym f(;kw...)
+    @test f(;a=a, hello="world", kw...) == @eponym f(;a, hello="world", kw...)
+    
+    @test_throws ArgumentError E.eponymmacro(:((a)))
+    @test_throws ArgumentError E.eponymmacro(:((a=1; b)))
 end
